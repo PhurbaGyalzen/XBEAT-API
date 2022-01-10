2ceb157f2858f831aa8eb1182d785a1208a512d9
PhurbaGyalzen <sphurba861@gmail.com>
2022-01-10 19:12:24 +0545

    User Model created

---
+++ models/User.js
@@ -0,0 +1,72 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  profile: {
+    type: String,
+    required: false,
+  },
+  description: {
+    type: String,
+    required: false,
+  },
+  followers: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  following: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  likedSongs: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Song",
+    },
+  ],
+  comments: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Comment",
+    },
+  ],
+  isAdmin: {
+    type: Boolean,
+    required: true,
+    default: false, // default value
+  },
+  isBlocked: {
+    type: Boolean,
+    required: true,
+    default: false, // default value
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
