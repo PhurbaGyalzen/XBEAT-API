61dd8426c693130d50cfe9159a59fe8727f0828d
PhurbaGyalzen <sphurba861@gmail.com>
2022-01-10 19:17:06 +0545

    Album Model created

---
+++ models/Album.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+const albumSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  artist: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Artist",
+  },
+  songs: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Song",
+    },
+  ],
+  description: {
+    type: String,
+    required: false,
+  },
+  genre: {
+    type: String,
+    required: false,
+  },
+  likes: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  comments: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Comment",
+    },
+  ],
+});
+
+const Album = mongoose.model("Album", albumSchema);
+
+export default Album;
