10841fb74bd63a1e1d235a9390bd539134d82f22
PhurbaGyalzen <sphurba861@gmail.com>
2022-01-17 18:20:25 +0545

    cors package added

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import connectDB from "./db/connectDB.js";
 import web from "./routes/web.js";
 import helmet from "helmet";
 import dotenv from "dotenv";
+import cors from "cors";
 dotenv.config(); // to read env variables
 
 // connecting to the database
@@ -12,6 +13,7 @@ connectDB(DATABASEURL); // connect to the database
 
 // initializing express
 const app = express(); // create an instance of express
+app.use(cors()); // to allow cross origin requests
 app.use(helmet()); // for security purpose
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
@@ -19,6 +21,5 @@ app.use("/", web); // for routing
 
 // starting the server
 app.listen(port, () => {
-
   console.log(`Server run at http://localhost:${port}`);
 });
